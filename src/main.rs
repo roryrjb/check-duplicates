dd5cfdc23ea955415b7ac342f020c98620e9cf45
Rory Bradford <roryrjb@gmail.com>
2022-10-15 10:57:21 +0100

    Initial commit
    Initial dumb implementation just to placeholder some ideas. Currently
    terribly inefficient as we are reading each file into memory and only in
    a single thread.

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use crc::{Crc, CRC_16_IBM_SDLC};
+use std::collections::HashMap;
+use std::env::current_dir;
+use std::fs::read_dir;
+
+pub const X25: Crc<u16> = Crc::<u16>::new(&CRC_16_IBM_SDLC);
+
+fn main() -> Result<(), std::io::Error> {
+    // simple hash map to store <hash as u16, full path to file as string>
+    let mut file_map: HashMap<u16, String> = HashMap::new();
+
+    for entry in read_dir(current_dir()?)? {
+        let path_buf = entry?.path();
+        let path = path_buf.to_str().unwrap();
+
+        match std::fs::read(path) {
+            Ok(bytes) => {
+                // TODO: don't read the whole file into memory
+                let hash = X25.checksum(&bytes);
+
+                // TODO: only print if verbose is set
+                // TODO: print to stderr
+                println!("{}: {:?}", path, hash);
+
+                // TODO: check if hash already exists, if it does, stat
+                // both files and check if they are really the same file
+                // under the assumption that the hash is going to collide
+                // often
+
+                file_map.insert(hash, String::from(path));
+            }
+            Err(_) => {}
+        };
+    }
+
+    Ok(())
+}
